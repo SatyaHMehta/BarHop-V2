7af0a3785a99384638d213f1efffc08f580be32f
mona <csimonusa@outlook.com>
2022-02-10 11:10:14 -0500

    whatwhat

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const session = require('express-session')
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(exphbs.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express()
 const PORT = process.env.PORT || 3001
