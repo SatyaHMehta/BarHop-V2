67f203c0a1512d022ce3606036451aa2dc6629b5
mona <csimonusa@outlook.com>
2022-02-13 10:06:19 -0500

    random

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 // const { User, blogPost} = require('../models');
 const withAuth = require('../utils/auth');
+const blogPost = require('../models/blogPost');
 
 router.get('/', async (req, res) => {
   try {
@@ -36,74 +37,42 @@ router.get('/homepage', async (req, res) => {
   }
 })
 
-router.get('/blogPost/:id', async (req, res) => {
-  try {
-    const blogData = await blogPost.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const blogPost = blogData.get({ plain: true });
-
-    res.render('blogPost', {
-      ...blogPost,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: blogPost }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
-router.get('/search', (req, res) => {
-
-  res.render('search');
-});
-
-router.get('/search', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
- 
-  res.render('search');
-});
-
-router.get('/post', async (req, res) => {
-  res.render('post')
-})
-
-router.get('/review', async (req, res) => {
-  res.render('review')
-})
+// router.get('/blogPost/:id', async (req, res) => {
 //   try {
-//     const blogData = await blogPost.findAll()
+//     const blogData = await blogPost.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
 
-//     const blogPost = blogData.map((project) => project.get({ plain: true }));
+//     const blogPost = blogData.get({ plain: true });
 
-//     res.render('post', {
-//       ...blogPost
+//     res.render('blogPost', {
+//       ...blogPost,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// // Use withAuth middleware to prevent access to route
+// router.get('/profile', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: blogPost }],
+//     });
+
+//     const user = userData.get({ plain: true });
+
+//     res.render('profile', {
+//       ...user,
+//       logged_in: true
 //     });
 //   } catch (err) {
 //     res.status(500).json(err);
@@ -111,6 +80,48 @@ router.get('/review', async (req, res) => {
 // });
 
 
+router.get('/search', (req, res) => {
+
+  res.render('search');
+});
+
+// router.get('/search', (req, res) => {
+//   // If the user is already logged in, redirect the request to another route
+ 
+//   res.render('search');
+// });
+
+// router.get('/post', async (req, res) => {
+//   res.render('post')
+// })
+
+router.get('/review', async (req, res) => {
+  res.render('review')
+})
+
+
+// router.get('', async(req, res) => {
+
+// })
+
+
+
+router.get('/post', async (req, res) => {
+  // res.render('post', {blogPost})
+  // console.log('hello')
+  try {
+  const blogs = await blogPost.findAll({
+    // include: [User]
+  })
+  console.log(blogs)
+
+  const blog = blogs.map((blog) => blog.get({ plain: true }));
+  // }).then((blogPost) => res.json(blogPost))
+  res.render('post', { blog })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
 
 
 
