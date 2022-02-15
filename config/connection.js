7ae7065f88c12cae3823f2d172b003789497895f
rsanand2202 <richa.anand0222@gmail.com>
2022-02-14 22:08:50 -0500

    changes pulling

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL)
   } else {
     sequelize = new Sequelize(
-      "Barhop_db",
-      "root",
-     "Rootroot15",
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
       {
         host: 'localhost',
         dialect: 'mysql',
